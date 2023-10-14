16f5767a7c74aee0b6d766a4b8e294294c2ae400
Cherry <60113072+Ch3rr13@users.noreply.github.com>
2023-10-14 17:37:43 -0400

    Added permissions and APIs for items - To be removed later once database is added

---
+++ app/src/main/java/com/example/coenelec390/ui/API.java
@@ -0,0 +1,22 @@
+package com.example.coenelec390.ui;
+
+import com.example.coenelec390.ui.item.Item;
+
+import java.util.List;
+
+import okhttp3.ResponseBody;
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.GET;
+import retrofit2.http.POST;
+import retrofit2.http.Path;
+public interface API {
+    @GET("product/")
+    Call<List<Item>> getProducts();
+
+    @POST("product/add/")
+    Call<ResponseBody> addProduct(@Body Item p);
+
+    @POST("product/update/{id}")
+    Call<ResponseBody> updateProduct(@Path("id") long productId, @Body Item p);
+}
